1134c5399bec17d221925fcede63407b89690f3f
antoniocolucci <83610915+antoniocolucci@users.noreply.github.com>
2021-05-07 00:02:12 +0200

    Add files via upload

--- Unimensa/js/Home.js
+++ Unimensa/js/Home.js
@@ -1,7 +1,6 @@
-function swapMenu (element_id) {
+function showContainer(element_id) {
 
     let element = document.getElementById(element_id)
-
     if(element.style.display === 'none') {
         element.style.display = 'block'
     } else {
@@ -9,10 +8,56 @@ function swapMenu (element_id) {
     }
 }
 
-/*function toggle_div_visibility (div_id) {
+function changeIcon(element_id){
+    let firstOcc = document.getElementById(element_id).src.indexOf('images')
+    let var2 = document.getElementById(element_id).src.substr(firstOcc,document.getElementById(element_id).src.length)
+    let var1 = "../"+var2
+    if(var1 === "../images/"+element_id+".png")
+     {
+        document.getElementById(element_id).src = "../images/"+element_id+"clicked.png"
+     }
+    else
+     {
+        document.getElementById(element_id).src = "../images/"+element_id+".png"
+     }
 
-    toggle_element_visibility(div_id+'_content')
-    toggle_element_visibility(div_id+'_button_hide')
-    toggle_element_visibility(div_id+'_button_show')
+}
 
-}*/
+function hideOtherMenu (element_id) {
+    let element = document.getElementById(element_id)
+    let nameContainer = [
+        'container_notifications',
+        'container_profile',
+        'lateral_menu'
+    ]
+
+    if(element == document.getElementById(nameContainer[0]))
+     {
+         if(document.getElementById(nameContainer[1]).style.display == 'block') {
+             document.getElementById(nameContainer[1]).style.display = 'none'
+             changeIcon('imgProfile')
+         }
+        document.getElementById(nameContainer[2]).style.display= 'none'
+     }
+
+    else if (element == document.getElementById(nameContainer[1]))
+     {
+         if(document.getElementById(nameContainer[0]).style.display == 'block') {
+             document.getElementById(nameContainer[0]).style.display= 'none'
+             changeIcon('imgBell')
+         }
+         document.getElementById(nameContainer[2]).style.display= 'none'
+     }
+
+    else
+     {
+         if(document.getElementById(nameContainer[0]).style.display == 'block') {
+             document.getElementById(nameContainer[0]).style.display= 'none'
+             changeIcon('imgBell')
+         }
+         if(document.getElementById(nameContainer[1]).style.display == 'block') {
+             document.getElementById(nameContainer[1]).style.display= 'none'
+             changeIcon('imgProfile')
+         }
+     }
+}
