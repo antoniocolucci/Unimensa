e3d836d628010397011fcdc81e796a57ad811ae8
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-05-25 10:36:15 +0200

    Update JQuery

--- Unimensa/js/JQuery.js
+++ Unimensa/js/JQuery.js
@@ -3,5 +3,17 @@ $(document).ready(function(){
     $('.liPlate').click(function(){
         $('.liPlate').removeClass("liActive");
         $(this).addClass("liActive");
+
     });
+
 });
+
+
+/*$(document).ready(function(){
+    $('.plate').click(function(){
+        $('.plate').not(this).removeClass("cardActive")
+        $(this).toggleClass("cardActive")
+
+    });
+
+});*/
