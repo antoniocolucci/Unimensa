fd02a836e973085ae94acab6b4581e8e53c9d4d7
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-05-26 16:53:27 +0200

    Update JavaScript

--- Unimensa/js/JQuery.js
+++ Unimensa/js/JQuery.js
@@ -7,3 +7,10 @@ $(document).ready(function(){
 
 });
 
+
+
+
+$('input[type="checkbox"]').on('change', function() {
+    $(this).siblings('input[type="checkbox"]').not(this).prop('checked', false);
+});
+
