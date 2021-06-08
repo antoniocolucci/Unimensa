1dfd830019d7b2b99498681ce89a6d34d9b78af7
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-06-08 23:28:01 +0200

    Update

--- Unimensa/app.py
+++ Unimensa/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import Flask, render_template, request, redirect, jsonify, url_for, session
 from flask_cors import CORS
 from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
 from passlib.hash import pbkdf2_sha256
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -13,9 +14,25 @@ cors = CORS(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    login()
+    if request.method == 'POST':
+        usr_email = request.form['Email']
+        usr_pwd = request.form['Password']
+        user = users.find_one({'Email': usr_email}).count()
+        print(user)
+        if user > 0:
+            user_rs = users.find_one({'Email': usr_email})
+            pwd = user_rs['Password']
+            print(pwd)
+            #if check_password_hash(pwd, usr_pwd):
+            session['sessionusername'] = usr_email
+            return redirect('/Home')
+            #else:
+             #   error = 'Invalid login'
+             #   return render_template('index.html', error=error)
+        else:
+            error = 'Username not found'
+            return render_template('index.html', error=error)
     return render_template('index.html')
-
 
 
 @app.route('/Home')
@@ -23,11 +40,18 @@ def home():
     return render_template('Home.html')
 
 
-def login():
-    user = users.find_one(
-        {"Email": request.form.get("Email")}
-    )
-    if(user and pbkdf2_sha256.verify(request.form.get("Password"), user['Password'])):
-        return jsonify({user})
-    else:
-        return jsonify({'error': 'Login Error'}), 400
+
+
+
+
+
+
+
+
+
+
+
+""" if(user and pbkdf2_sha256.verify(request.form.get("Password"), user['Password'])):
+            return jsonify({user})
+        else:
+            return jsonify({'error': 'Login Error'}), 400"""
