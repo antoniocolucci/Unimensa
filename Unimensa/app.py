6b3178c89e5636b12c43aa40c26aeca17e5a87d3
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-06-09 19:24:15 +0200

    Update

--- Unimensa/app.py
+++ Unimensa/app.py
@@ -9,9 +9,10 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["unimensa"]
 users = db["users"]
 app = Flask(__name__)
+app.secret_key = 'unimensakey'
 cors = CORS(app)
 
-#CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 
 
@@ -19,8 +20,64 @@ cors = CORS(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        allusers = users
+        login_user = allusers.find_one({"Email": request.form['Email']})
+        if login_user:
+            if pbkdf2_sha256.verify(request.form['Password'], login_user['Password']):
+                session["_id"] = login_user["_id"]
+                session["Type"] = login_user["Type"]
+                return redirect('Home')
+        return "Login error"
+    return render_template('index.html')
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    session.pop('_id', None)
+    session.pop("Type", None)
+    return redirect(url_for('index'))
+
+
+@app.route('/Home', methods=['POST', 'GET'])
+def home():
+    if session["Type"] == "admin":
+        return render_template('home_00.html')
+    else:
+        return render_template('Home.html')
+
+@app.route('/myAccount')
+def account():
+    return render_template('Account.html')
+
+@app.route('/Order')
+def order():
+    return render_template('Order.html')
+
+@app.route('/Cartphone')
+def cartphone():
+    return render_template('Cart.html')
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.secret_key = 'unimensa'
+    app.run(debug=True)
+
+
+
+""" if(user and pbkdf2_sha256.verify(request.form.get("Password"), user['Password'])):
+            return jsonify({user})
+        else:
+            return jsonify({'error': 'Login Error'}), 400
+            
+                if request.method == 'POST':
         usr_email = request.form['Email']
         usr_pwd = request.form['Password']
+
         user = users.find_one({'Email': usr_email}).count()
         print(user)
         if user > 0:
@@ -38,28 +95,4 @@ def index():
             return render_template('index.html', error=error)
     return render_template('index.html')
 
-@app.route('/logout', methods=['POST', 'GET'])
-def logout():
-    session.pop('_id', None)
-    session.pop("Type", None)
-    return redirect(url_for('index'))
-
-
-@app.route('/Home')
-def home():
-    return render_template('Home.html')
-
-
-
-
-
-if __name__ == '__main__':
-    app.secret_key = 'unimensa'
-    app.run(debug=True)
-
-
-
-""" if(user and pbkdf2_sha256.verify(request.form.get("Password"), user['Password'])):
-            return jsonify({user})
-        else:
-            return jsonify({'error': 'Login Error'}), 400"""
+            """
--- Unimensa/create_users.py
+++ Unimensa/create_users.py
@@ -4,11 +4,11 @@ import uuid
 
 user = [
 
-    {"_id": uuid.uuid4().hex, "Name": "Antonio", "Surname": "Colucci", "Email": "antoniocolucci@universita.it", "Password": pbkdf2_sha256.hash("antoniocolucci")},
-    {"_id": uuid.uuid4().hex, "Name": "Antonio", "Surname": "Sarno", "Email": "antoniosarno@universita.it", "Password": pbkdf2_sha256.hash("antoniosarno")},
-    {"_id": uuid.uuid4().hex, "Name": "Jonathan", "Surname": "De Michele", "Email": "jonathandemichele@universita.it", "Password": pbkdf2_sha256.hash("jonathandemichele")},
-    {"_id": uuid.uuid4().hex, "Name": "Mario", "Surname": "Rossi", "Email": "mariorossi@docenti.universita.it", "Password": pbkdf2_sha256.hash("mariorossi")},
-    {"_id": uuid.uuid4().hex, "Name": "Angelo", "Surname": "Bianchi", "Email": "angelobianchi@personale.it", "Password": pbkdf2_sha256.hash("angelobianchi")}
+    {"_id": uuid.uuid4().hex, "Name": "Antonio", "Surname": "Colucci", "Email": "antoniocolucci@universita.it", "Password": pbkdf2_sha256.hash("antoniocolucci"), "Type": "user"},
+    {"_id": uuid.uuid4().hex, "Name": "Antonio", "Surname": "Sarno", "Email": "antoniosarno@universita.it", "Password": pbkdf2_sha256.hash("antoniosarno"), "Type": "user"},
+    {"_id": uuid.uuid4().hex, "Name": "Jonathan", "Surname": "De Michele", "Email": "jonathandemichele@universita.it", "Password": pbkdf2_sha256.hash("jonathandemichele"), "Type": "user"},
+    {"_id": uuid.uuid4().hex, "Name": "Mario", "Surname": "Rossi", "Email": "mariorossi@docenti.universita.it", "Password": pbkdf2_sha256.hash("mariorossi"), "Type": "user"},
+    {"_id": uuid.uuid4().hex, "Name": "Angelo", "Surname": "Bianchi", "Email": "angelobianchi@personale.it", "Password": pbkdf2_sha256.hash("angelobianchi"), "Type": "admin"}
 ]
 
 add = users.insert_many(user)
@@ -27,7 +27,11 @@ for item in result:
 """Aggiornare un singolo documento all'interno della collezione.
 old_values = {"Surname": "Insigne"}
 new_values = {"$set": {"Surname": "Immobile"}}
-collection.update_one(old_values, new_values)"""
+collection.update_one(old_values, new_values)
+
+"Password": pbkdf2_sha256.hash("angelobianchi")
+
+"""
 
 
 
