17db0a898db91d9016d0a22dbe7b6cbaa0965417
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-06-26 14:32:20 +0200

    Update app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, session, json
 from flask_cors import CORS
 from pymongo import MongoClient
+from flask_session import Session
+import requests
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from passlib.hash import pbkdf2_sha256
@@ -20,6 +22,10 @@ app.secret_key = 'unimensakey'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 cors = CORS(app)
 
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -27,7 +33,7 @@ def index():
         if login_user:
             if pbkdf2_sha256.verify(request.form['Password'], login_user['Password']):
                 session["_id"] = login_user["_id"]
-                session["Type"] = login_user["Type"]
+                session['Type'] = login_user['Type']
                 return redirect('Home')
             return render_template('index.html', error=1)
         return render_template('index.html', error=2)
@@ -48,23 +54,52 @@ def home():
             id = uuid.uuid4().hex
             pricePlate = request.form['Price']
             ingredients = request.form['Ingredients']
-            section =  request.form['Section_card']
+            section = request.form['Section_card']
             file = request.files['imgFile']
-            print('file ok')
             filename = secure_filename(file.filename)
-            print('filename ok')
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print('filesave ok')
             newCard = {'_id': id, 'Name': namePlate, 'Price': pricePlate, 'Ingredients': ingredients, 'Section': section, 'Filename': filename}
-            print('newCard ok')
             plate.insert_one(newCard)
-            print('insert ok')
             return redirect('Home')
         else:
             return render_template('home_00.html')
     else:
         return render_template('Home.html')
 
+@app.route('/LoadCard', methods=['GET', 'POST'])
+def loadCard():
+    if request.method == 'POST':
+        plates = plate.find({'Section': request.form['Section_card']})
+        if plates:
+            list_plates = list(plates)
+            result = {
+                        'plates': list_plates,
+                        'Type': session['Type']
+            }
+            return jsonify(result)
+        else:
+            return render_template('home_00.html')
+    return render_template('home_00.html')
+
+
+@app.route('/DeleteCard', methods=['POST', 'GET'])
+def deleteCard():
+    if request.method == 'POST':
+        data = request.get_data('Name')
+        #data = request.get_data()
+        print(data)
+        index = data.index('=')
+        id=data[(index+1):end]
+        plates = plate.find({'_id': data})
+        list_plates = list(plates)
+        #print(list_plates)
+        if plates:
+            #print(list_plates)
+            #plates = plate.delete_one({})
+            return redirect('Home')
+        else:
+            return render_template('home_00.html')
+    return render_template('home_00.html')
 
 @app.route('/myAccount')
 def account():
@@ -101,6 +136,10 @@ def signin():
                 return redirect(url_for('index'))
 
     return render_template('signin.html')
+
+
+
+
 
 
 if __name__ == '__main__':
