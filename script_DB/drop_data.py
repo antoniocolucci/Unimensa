f2b57cb55df4b3833967674ae377eeb49510d890
antoniocolucci <antonio.colucci003@studenti.uniparthenope.it>
2021-06-30 17:15:29 +0200

    Add script for Database

---
+++ script_DB/drop_data.py
@@ -0,0 +1,16 @@
+from app import db, users, plate, sandwiches
+from passlib.hash import pbkdf2_sha256
+import uuid
+
+
+# Drop data of users.
+result_users = users.find()
+result_users = users.delete_many({})
+
+# Drop data of plates.
+result_plates = plate.find()
+result_plates = plate.delete_many({})
+
+# Drop data of ingredients of sandwiches.
+result_sandwiches = sandwiches.find()
+result_sandwiches = sandwiches.delete_many({})
